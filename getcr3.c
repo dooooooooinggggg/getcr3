221a54f7ee69cdde309b69e1081ad59d73a636e8
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-12-11 15:56:22 +0900

    eax -> rax

--- getcr3.c
+++ getcr3.c
@@ -44,11 +44,11 @@ static ssize_t getcr3_read(struct file *filp, char __user *buf,
 	u64 cr0, cr2, cr3;
 	__asm__ __volatile__(
 		"mov %%cr0, %%rax\n\t"
-		"mov %%eax, %0\n\t"
+		"mov %%rax, %0\n\t"
 		"mov %%cr2, %%rax\n\t"
-		"mov %%eax, %1\n\t"
+		"mov %%rax, %1\n\t"
 		"mov %%cr3, %%rax\n\t"
-		"mov %%eax, %2\n\t"
+		"mov %%rax, %2\n\t"
 		: "=m"(cr0), "=m"(cr2), "=m"(cr3)
 		: /* no input */
 		: "%rax");
