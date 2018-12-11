801ccf09cb0d9987a32b03e53d83eb26a455efb4
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-12-11 13:48:36 +0900

    change format

--- getcr3.c
+++ getcr3.c
@@ -3,7 +3,7 @@
 #include <linux/fs.h>
 #include <linux/poll.h>
 #include <linux/string.h>
-#include <linux/wait.h>		/* wait_queue_head_t */
+#include <linux/wait.h> /* wait_queue_head_t */
 #include <linux/interrupt.h>
 #include <linux/version.h>
 #include <linux/mm.h>
@@ -16,76 +16,73 @@
 
 //#define	DEBUG
 
-#ifndef	DRV_NAME
-#define	DRV_NAME	"getcr3"
+#ifndef DRV_NAME
+#define DRV_NAME "getcr3"
 #endif
-#ifndef	DRV_IDX
-#define	DRV_IDX		(0)
+#ifndef DRV_IDX
+#define DRV_IDX (0)
 #endif
-#define	DRV_VERSION	"0.0.0"
-#define	getcr3_DRIVER_NAME	DRV_NAME " getcr3 driver " DRV_VERSION
+#define DRV_VERSION "0.0.0"
+#define getcr3_DRIVER_NAME DRV_NAME " getcr3 driver " DRV_VERSION
 
-#if LINUX_VERSION_CODE > KERNEL_VERSION(3,8,0)
-#define	__devinit
-#define	__devexit
-#define	__devexit_p
+#if LINUX_VERSION_CODE > KERNEL_VERSION(3, 8, 0)
+#define __devinit
+#define __devexit
+#define __devexit_p
 #endif
-
 
 static int getcr3_open(struct inode *inode, struct file *filp)
 {
 	printk("%s\n", __func__);
-
 	return 0;
 }
 
 static ssize_t getcr3_read(struct file *filp, char __user *buf,
-			   size_t count, loff_t *ppos)
+						   size_t count, loff_t *ppos)
 {
 #ifdef __x86_64__
 	u64 cr0, cr2, cr3;
-	__asm__ __volatile__ (
+	__asm__ __volatile__(
 		"mov %%cr0, %%rax\n\t"
 		"mov %%eax, %0\n\t"
 		"mov %%cr2, %%rax\n\t"
 		"mov %%eax, %1\n\t"
 		"mov %%cr3, %%rax\n\t"
 		"mov %%eax, %2\n\t"
-	: "=m" (cr0), "=m" (cr2), "=m" (cr3)
-	: /* no input */
-	: "%rax"
-	);
-	if ( copy_to_user( buf, (unsigned char *)&cr3, sizeof(u64) ) ) {
-		printk( KERN_INFO "copy_to_user failed\n" );
+		: "=m"(cr0), "=m"(cr2), "=m"(cr3)
+		: /* no input */
+		: "%rax");
+	if (copy_to_user(buf, (unsigned char *)&cr3, sizeof(u64)))
+	{
+		printk(KERN_INFO "copy_to_user failed\n");
 		return -EFAULT;
 	}
 	return sizeof(u64);
 #elif defined(__i386__)
 	u32 cr0, cr2, cr3;
-	__asm__ __volatile__ (
+	__asm__ __volatile__(
 		"mov %%cr0, %%eax\n\t"
 		"mov %%eax, %0\n\t"
 		"mov %%cr2, %%eax\n\t"
 		"mov %%eax, %1\n\t"
 		"mov %%cr3, %%eax\n\t"
 		"mov %%eax, %2\n\t"
-	: "=m" (cr0), "=m" (cr2), "=m" (cr3)
-	: /* no input */
-	: "%eax"
-	);
-	if ( copy_to_user( buf, (unsigned char *)&cr3, sizeof(u32) ) ) {
-		printk( KERN_INFO "copy_to_user failed\n" );
+		: "=m"(cr0), "=m"(cr2), "=m"(cr3)
+		: /* no input */
+		: "%eax");
+	if (copy_to_user(buf, (unsigned char *)&cr3, sizeof(u32)))
+	{
+		printk(KERN_INFO "copy_to_user failed\n");
 		return -EFAULT;
 	}
 	return sizeof(u32);
 #endif
 }
 
-
 static struct file_operations getcr3_fops = {
-	.owner		= THIS_MODULE,
-	.read		= getcr3_read,
-	.open		= getcr3_open,
+	.owner = THIS_MODULE,
+	.read = getcr3_read,
+	.open = getcr3_open,
 };
 
 static struct miscdevice getcr3_dev = {
@@ -105,7 +102,8 @@ static int __devinit getcr3_init(void)
 
 	/* register character device */
 	ret = misc_register(&getcr3_dev);
-	if (ret) {
+	if (ret)
+	{
 		printk("fail to misc_register (MISC_DYNAMIC_MINOR)\n");
 		goto error;
 	}
--- user.c
+++ user.c
@@ -6,25 +6,27 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/mman.h>
+#include <inttypes.h>
 //#define DEBUG
 
-#define	MEM_DEVICE	"/dev/getcr3"
-#define	BUF_SIZE	(256)
+#define MEM_DEVICE "/dev/getcr3"
+#define BUF_SIZE (256)
 
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
 	char buf[8];
-	long long cr3;
+	uint64_t cr3;
 	int fd, rc, i;
-	if ((fd=open(MEM_DEVICE,O_RDONLY)) <0) {
-		fprintf(stderr,"cannot open %s\n",MEM_DEVICE);
+	if ((fd = open(MEM_DEVICE, O_RDONLY)) < 0)
+	{
+		fprintf(stderr, "cannot open %s\n", MEM_DEVICE);
 		return 1;
 	}
-
-	while (1) {
-		rc = read(fd, &cr3, 8);
-		printf("return code(length)=%d  CR3=%llX\n", rc, cr3);
-		sleep( 1 );
+	while (1)
+	{
+		rc = read(fd, &cr3, 8); // cr3という変数に，8Byte読み込む
+		printf("return code(length)=%d  CR3=0x%PRIx64\n", rc, cr3);
+		sleep(1);
 	}
 
 	close(fd);
